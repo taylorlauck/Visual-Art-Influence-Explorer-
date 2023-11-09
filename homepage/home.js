145277ef619dce368a57f8b897fcca56f41066ed
Taylor <taylorlauck%@gmail.com>
2023-11-08 19:14:07 -0500

    first commit

---
+++ homepage/home.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Fetch art data from the Artsy API
+    fetchArtData();
+  
+    function fetchArtData() {
+      // Make an API request to retrieve art data from Artsy
+      // Handle the response and display the art on the page
+      // You can use the fetch() API for this purpose
+    }
+  });
+  
